1fcdfe4f524ba93f4b26e7e560e1f7de01d31624
aziz <rahul@ziz.local>
2026-08-18 12:42:57 +0530

    Bind to 0.0.0.0 for deployment, disable debug mode

--- app.py
+++ app.py
@@ -262,4 +262,4 @@ def city_data(city):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5001)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=False)
